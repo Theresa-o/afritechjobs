f5d725353fdf79fe733b2f4b1f8e87fbd4cc883e
Theresa-o <62052443+Theresa-o@users.noreply.github.com>
2023-05-30 04:52:41 -0700

    created serializer

--- afritechjobsapi/admin.py
+++ afritechjobsapi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile, Blog, Event, WorkResources
+
+admin.site.register(Profile)
+admin.site.register(Blog)
+admin.site.register(Event)
+admin.site.register(WorkResources)
