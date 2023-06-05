93ad42ad9364721efe23752ed7d9e7fbac1a05cd
Theresa-o <62052443+Theresa-o@users.noreply.github.com>
2023-06-05 13:14:13 -0700

    Created PostAJob model

--- afritechjobsapi/models.py
+++ afritechjobsapi/models.py
@@ -99,6 +99,46 @@ class HiringGuide(models.Model):
     def __str__(self):
         return f"{self.title} by {self.author}"
     
+class JobSkills(models.Model):
+    #more things to be added
+    #skills options should change based on the selected category
+    #https://stackoverflow.com/questions/3582544/django-model-choice-option-as-a-multi-select-box and https://stackoverflow.com/questions/15393134/django-how-can-i-create-a-multiple-select-form
+    SKILLS_CHOICES = (
+        ('Django', 'Django'),
+        ('React', 'React'),
+    )
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20, choices=SKILLS_CHOICES)
+    category = models.ManyToManyField(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class JobLocation(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class PostAJob(models.Model):
+    job_title = models.CharField(max_length=200)
+    job_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    job_skills = models.ManyToManyField
+    job_salary_range = models.IntegerField(blank=True)
+    job_type = models.ForeignKey(JobSkills, on_delete=models.SET_NULL)
+    job_description = models.TextField()
+    # job_type = 
+    job_location = models.ForeignKey(JobLocation, on_delete=models.SET_NULL)
+    # job_level
+    # job_application_link
+    # company_name
+    # company_hq
+    # company_logo
+    # companys_website
+    # company_contact_email
+    # company_description
+
+    
 
 
 
