bfe5343dbe49fa6b67717739cf900204a4e37a80
Theresa-o <62052443+Theresa-o@users.noreply.github.com>
2023-07-03 10:02:47 -0700

    Separate apps for candidates, recruiters & admins

--- afritechjobs/settings.py
+++ afritechjobs/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'afritechjobsapi'
+    'afritechjobsapi',
+    'candidates',
+    'recruiters',
+    'admin'
 ]
 
 MIDDLEWARE = [
